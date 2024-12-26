114a9715ffe5bba73bbe29d9bff00ac4374c194b
manhcuong03 <154748535+manhcuong03@users.noreply.github.com>
2024-12-26 22:54:50 +0700

    Delete UI directory

--- UI/loginHandle.py
+++
@@ -1,7 +0,0 @@
-from login import Ui_MainWindow
-
-class LOGIN_HANDLE(Ui_MainWindow):
-    def __init__(self, mainwindow):
-        self.setupUi(mainwindow)
-
-        #--------------------------------
